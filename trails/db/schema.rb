520ef6ce5fd200a1e3023e079a2cbff6990f3c13
Min McKnight <min.mcknight@gmail.com>
2015-01-29 01:31:41 -0500

    added links to the pictures to save it to the trail database

--- trails/db/schema.rb
+++ trails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127005212) do
+ActiveRecord::Schema.define(version: 20150129050651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20150127005212) do
   create_table "trails", force: :cascade do |t|
     t.string   "name"
     t.string   "url"
-    t.string   "geocode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "lon"
+    t.decimal  "lat"
   end
 
   create_table "users", force: :cascade do |t|
