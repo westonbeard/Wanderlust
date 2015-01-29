cecc05757423ffb3835722d552c11b6c79331e10
mattcho <dongukcho@gmail.com>
2015-01-28 23:45:52 -0500

    add address column to users

--- trails/app/models/user.rb
+++ trails/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :trail_users, dependent: :destroy
   has_many :trails, :through => :trail_users
   validates :email, presence: true, uniqueness: true
+
+  attr_accessible :address, :latitude, :longitude
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
--- trails/db/schema.rb
+++ trails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129012059) do
+ActiveRecord::Schema.define(version: 20150129044514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150129012059) do
     t.datetime "updated_at",      null: false
     t.decimal  "latitude"
     t.decimal  "longitude"
+    t.string   "address"
   end
 
 end
