fbab95bddb525af9e78a5b508b8869a078062d1f
mattcho <dongukcho@gmail.com>
2015-01-29 09:00:26 -0500

    rake db:migrate

--- trails/db/schema.rb
+++ trails/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150129044514) do
+ActiveRecord::Schema.define(version: 20150129050651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 20150129044514) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.decimal  "lon"
+    t.decimal  "lat"
   end
 
   create_table "users", force: :cascade do |t|
