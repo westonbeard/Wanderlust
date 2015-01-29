cb9eb4deb36cb3cbccb01a046e2cf1c29d084b0f
mattcho <dongukcho@gmail.com>
2015-01-29 02:45:34 -0500

    driving routes working

--- trails/app/models/user.rb
+++ trails/app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   has_many :trails, :through => :trail_users
   validates :email, presence: true, uniqueness: true
 
-  attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 end
